c92de3beb2f1ae93de41f488b6e5f8b2ab06b747
tsdong <dts0706@163.com>
2016-11-02 11:58:42 +0800

    first commit

---
+++ spec/json-tree-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JsonTreeView from '../lib/json-tree-view';
+
+describe('JsonTreeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
